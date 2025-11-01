57bbdd89e9c40e544a2ae0835879cd717e9cad24
bjnzoom2 <bjnzoom2@gmail.com>
2025-11-01 16:24:47 +0800

    Initialized window

--- main.cpp
+++ main.cpp
@@ -7,7 +7,50 @@
 #include <imgui_impl_opengl3.h>
 #include "gl2d.h"
 
+struct gameData {
+
+};
+
+gl2d::Renderer2D renderer;
+
+unsigned int windowWidth = 800;
+unsigned int windowHeight = 800;
+
+bool gameLogic() {
+	
+	return true;
+}
+
 int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Atom Sim", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to load window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	gl2d::init();
+	renderer.create();
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	renderer.cleanup();
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
