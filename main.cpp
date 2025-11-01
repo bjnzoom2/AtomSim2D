4e93add702a8008e18e1a80ff557c2dc5842e561
bjnzoom2 <bjnzoom2@gmail.com>
2025-11-01 16:08:44 +0800

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include "gl2d.h"
+
+int main() {
+
+	return 0;
+}
